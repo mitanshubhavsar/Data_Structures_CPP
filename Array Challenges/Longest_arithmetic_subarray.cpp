4c69b05080e7b43299136be26ac3086ae9116c16
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-03-30 01:23:13 +0530

    Longest Aritheetic subarray

---
+++ Array Challenges/Longest_arithmetic_subarray.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int ans = 2;
+    int d = arr[1] - arr[0];
+    int j = 2;
+    int curr = 2;
+
+    while (j<n)
+    {
+        if(arr[j] - arr[j-1]==d){
+            curr++;
+        }
+        else {
+            d = arr[j] - arr[j-1];
+            curr=2;
+        }
+        ans = max(ans,curr);
+        j++;
+    }
+
+    cout<<ans<<endl;
+    
+
+    return 0;
+}
