1d014803a053f71e4bc50892b19f33ab988b8ca5
Mitanshu Bhavsar <mitanshubhavsar90@gmail.com>
2021-04-04 00:02:32 +0530

    All possible words from phone digits

---
+++ Recursion Challenges/7 Best Problems/keypad.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+string keypadArr[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+void keypad(string s, string ans){
+    if(s.length()==0){
+        cout<<ans<<endl;
+        return;
+    }
+
+    char ch = s[0];
+    string code = keypadArr[ch-'0'];
+    string rest_of_string = s.substr(1);
+
+    for(int i=0;i<code.length();i++){
+        keypad(rest_of_string,ans+code[i]);
+    }
+}
+
+int main(){
+
+    keypad("23","");
+
+    return 0;
+}
